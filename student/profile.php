4986e10b20c5e3587600cb6a570e44f6d39ac16b
somyav1197 <somyavashistha@gmail.com>
2018-07-03 18:22:09 +0530

    files added

---
+++ student/profile.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+    <link rel="stylesheet" href="../asset/css/custom.css">
+    <style>
+<p border-style: double;">
+    img {
+        border: 1px solid #ddd;
+        border-radius: 4px;
+        padding: 5px;
+        width: 200px;
+        height: 200px;
+    }
+
+    img:hover {
+        box-shadow: 0 0 2px 1px rgba(0, 140, 186, 0.5);
+    }
+    </style>
+  </head>
+  <body>
+
+
+<img src="../images/author.jpg" alt="dummy" style="width:200px">
+
+  </body>
+</html>
---
+++ tutor/student_profile.php
@@ -0,0 +1,13 @@
+<?php
+
+session_start();
+require('../config/dbconnect.php');
+$id = $_GET['id'];
+$query = mysqli_query($conn, "SELECT * FROM tutor_reg WHERE id = '$id'");
+$array = mysqli_fetch_array($query);
+
+
+ ?>
+
+
+name of tutor: <?php echo $array['first_name']." ".$array['second_name']; ?>
